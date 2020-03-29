9c685d5d62ff0d27a2af6b101758fdbe12b41179
xushulong <610739389@qq.com>
2020-03-29 18:20:30 +0800

    测试提交

---
+++ app/src/main/java/com/example/trading_android/activity/LoginActivity.java
@@ -0,0 +1,138 @@
+package com.example.trading_android.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.trading_android.R;
+import com.example.trading_android.URLpath;
+import com.example.trading_android.model.ServerResponse;
+import com.example.trading_android.model.User;
+import com.example.trading_android.tableView.MainActivity;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class LoginActivity extends AppCompatActivity {
+    private  static final int UPDATE_TECT = 1 ;
+    private  static final int Tent_TECT = 2 ;
+    private static final String TAG = "LoginActivity";
+    private Button buttonLogin;
+    private EditText accounttext;     //输入账号框的信息
+    private String account;
+    private EditText pwdtext;         //输入密码框的信息
+    private String pwd;
+    private ServerResponse<User> serverResponse;
+    private  User user;
+
+
+    private Handler mHandler = new Handler(){
+
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case UPDATE_TECT:
+                    Toast.makeText(LoginActivity.this,"登录成功！！！",Toast.LENGTH_LONG).show();
+                    break;
+                case   Tent_TECT:
+                    Toast.makeText(LoginActivity.this,"账号不存在或密码错误！！！",Toast.LENGTH_LONG).show();
+                    break;
+                default:
+                    break;
+            }
+        };
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            actionBar.hide();
+        }
+
+        buttonLogin = findViewById(R.id.btn_login);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                accounttext = findViewById(R.id.input_mobile);
+                account = accounttext.getText().toString();
+                pwdtext = findViewById(R.id.input_password);
+                pwd = pwdtext.getText().toString();
+
+
+                sendRequestwithOKhttp();
+
+
+            }
+        });
+
+    };
+
+    private void sendRequestwithOKhttp() {
+        final String urlLogin = URLpath.BASE_URL+URLpath.LOGIN_URL;
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    OkHttpClient client = new OkHttpClient();
+                    RequestBody requestBody = new FormBody.Builder()
+                            .add("userName",account)
+                            .add("password",pwd)
+                            .build();
+                    Request request =new Request.Builder()
+                            .url(urlLogin)
+                            .post(requestBody)
+                            .build();
+                    Response response =client.newCall(request).execute();
+                    String  responsedate = response.body().string();
+                    Log.d(TAG,responsedate);
+                    parseJSNOWITHgson(responsedate);
+
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+    }
+
+    private void parseJSNOWITHgson(String responsedate) {
+        Gson gson = new  Gson();
+        serverResponse = gson.fromJson(responsedate,new TypeToken<ServerResponse<User>>(){}.getType());
+        Message message = new Message();
+        if(serverResponse.getStatu() ==200){
+            user = (User) serverResponse.getData();
+            message.what = UPDATE_TECT;
+            mHandler.sendMessage(message);
+            String value = String.valueOf(user.getUid());
+            Bundle mBundle = new Bundle();
+            mBundle.putString("id",value);
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            intent.putExtras(mBundle);
+            Log.d("test12311111111", "许舒隆"+value);
+            startActivity(intent);
+            finish();
+        }else{
+            message.what = Tent_TECT;
+            mHandler.sendMessage(message);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/trading_android/tableView/Fragment1.java
@@ -0,0 +1,46 @@
+package com.example.trading_android.tableView;
+
+import com.xuexiang.xui.widget.banner.widget.banner.*;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.trading_android.R;
+
+import java.util.List;
+
+
+public class Fragment1 extends Fragment {
+
+    private Button button;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle
+            savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_item1, null);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        button =(Button)getActivity().findViewById(R.id.buttomView1);
+        button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(),"test",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
+}
